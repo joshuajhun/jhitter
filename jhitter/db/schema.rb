84d698987497bb5c054417b49dc7e960477c9b7d
joshuajhun <joshuajhun@gmail.com>
2015-12-17 11:44:28 -0700

    got log in test to work and current user styles implemented

--- jhitter/db/schema.rb
+++ jhitter/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215000234) do
+ActiveRecord::Schema.define(version: 20151217092226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20151215000234) do
     t.string   "oauth_token_secret"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "description"
+    t.string   "profile_picture"
+    t.string   "location"
+    t.string   "background_picture"
+    t.integer  "followers"
+    t.integer  "following"
+    t.integer  "likes"
+    t.integer  "tweet_count"
   end
 
 end
--- jhitter/test/integration/o_auth_user_test.rb
+++ jhitter/test/integration/o_auth_user_test.rb
@@ -6,18 +6,16 @@ class OAuthUserTest < ActionDispatch::IntegrationTest
     click_link 'login'
     assert_equal 200, page.status_code
     assert_equal timeline_index_path, current_path
-    assert page.has_content?('joshuajhun')
+    assert page.has_content?('jhitter')
     assert page.has_link?('logout')
   end
 
   test" a user can logout" do
-    skip
     visit root_path
     click_link "login"
     assert_equal 200, page.status_code
-    click_link 'login'
     assert_equal timeline_index_path, current_path
-    assert page.has_content?('joshuajhun')
+    assert page.has_content?('jhitter')
     assert page.has_link?('logout')
     click_link "logout"
     assert_equal root_path, current_path
