0c34e23e539221f7c1e19a49e742fdd3d17d9400
joshuajhun <joshuajhun@gmail.com>
2015-12-16 23:21:46 -0700

    test are failing

--- jhitter/test/integration/o_auth_user_test.rb
+++ jhitter/test/integration/o_auth_user_test.rb
@@ -2,22 +2,24 @@ require 'test_helper'
 
 class OAuthUserTest < ActionDispatch::IntegrationTest
   test "logging in works" do
-    visit '/'
-    assert_equal 200, page.status_code
+    visit root_path
     click_link 'login'
-    assert_equal '/', current_path
+    assert_equal 200, page.status_code
+    assert_equal timeline_index_path, current_path
     assert page.has_content?('joshuajhun')
     assert page.has_link?('logout')
   end
 
   test" a user can logout" do
-    visit '/'
+    skip
+    visit root_path
+    click_link "login"
     assert_equal 200, page.status_code
     click_link 'login'
-    assert_equal '/', current_path
+    assert_equal timeline_index_path, current_path
     assert page.has_content?('joshuajhun')
     assert page.has_link?('logout')
     click_link "logout"
     assert_equal root_path, current_path
-  end 
+  end
 end
--- jhitter/test/test_helper.rb
+++ jhitter/test/test_helper.rb
@@ -1,3 +1,4 @@
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
@@ -6,6 +7,8 @@ require 'capybara/rails'
 require 'pry'
 require 'webmock'
 require 'vcr'
+require  'SimpleCov'
+SimpleCov.start
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -14,7 +17,8 @@ class ActiveSupport::TestCase
 
   VCR.configure do |config|
     config.cassette_library_dir = 'test/cassettes'
-    config.hook_into :webmock
+    config.hook_into(:webmock)
+    config.allow_http_connections_when_no_cassette = true
   end
 
   def setup
@@ -25,17 +29,18 @@ class ActiveSupport::TestCase
   def stub_omniauth
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      uid: "4580803393",
       provider: 'twitter',
       extra: {
         raw_info: {
           user_id: "1234",
-          name: "joshuajhun",
-          screen_name: "worace",
+          name: "jhitter",
+          screen_name: "jhittertweets",
         }
       },
       credentials: {
-        token: "pizza",
-        secret: "secretpizza"
+        token: "4580803393-g1ckhlOgOIySt9vCzR6IgcCLU7wghvzOfJgHSoX",
+        secret: "qgRKEoS1RcgOK4yfZscjXdKIJ3Q6hHzohdyIt85EEL1UB"
       }
     })
   end
